38f35f809ffbe3eb9cf038bf75ed3bd0460e23ea
Robert Kruszewski <dzobert@gmail.com>
2013-05-13 01:02:08 -0700

    add shims for new libraries

--- client/app.js
+++ client/app.js
@@ -4,7 +4,10 @@ requirejs.config({
         "client": "client",
         "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
         "easeljs": "easeljs-0.6.0.min",
-        "bootstrap": "bootstrap"
+        "bootstrap": "bootstrap",
+        "three": "three.min",
+        "cannon": "cannon.min",
+        "box2d": "box2d"
     }
 });
 
